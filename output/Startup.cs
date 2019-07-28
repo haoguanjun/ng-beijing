d3bbd06f318aa71773eb163a5f62b27c984b8a54
haoguanjun <haoguanjun@gmail.com>
2019-07-28 23:19:39 +0800

    Update Startup.cs

--- output/Startup.cs
+++ output/Startup.cs
@@ -15,7 +15,8 @@ namespace dotnetcore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-           app.UseStaticFiles();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
